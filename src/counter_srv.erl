359680caccab537014561bd42d03dd51bc9b2f87
Ilya Beda <ir4y.ix@gmail.com>
2013-10-20 21:26:02 +0800

    Implement distributed pn_counter in counter_srv

--- src/counter_srv.erl
+++ src/counter_srv.erl
@@ -6,7 +6,10 @@
 %% API Function Exports
 %% ------------------------------------------------------------------
 
--export([start_link/0]).
+-export([start_link/0, 
+         increment/0, increment/1, 
+         decrement/0, decrement/1,
+         value/0, merge/1]).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Exports
@@ -22,16 +25,50 @@
 start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
+increment() ->
+    increment(1).
+increment(Amount) when is_integer(Amount), Amount > 0 ->
+    gen_server:cast(?SERVER, {increment, Amount}).
+
+decrement() ->
+    decrement(1).
+decrement(Amount) when is_integer(Amount), Amount > 0 ->
+    gen_server:cast(?SERVER, {decrement, Amount}).
+
+value() ->
+    gen_server:call(?SERVER, {get_value}).
+
+merge(GCounter) ->
+    gen_server:call(?SERVER, {merge, GCounter}).
+
 %% ------------------------------------------------------------------
 %% gen_server Function Definitions
 %% ------------------------------------------------------------------
 
-init(Args) ->
-    {ok, Args}.
+init([]) ->
+    %TODO load counter from local db
+    {ok, riak_dt_gcounter:new()}.
 
+handle_call({get_value}, _From, GCounter) ->
+    Positive = lists:sum([Amount || {{Sign, _Node} ,Amount} <- GCounter, Sign =:= p]),
+    Negative = lists:sum([Amount || {{Sign, _Node} ,Amount} <- GCounter, Sign =:= n]),
+    {reply, Positive - Negative, GCounter};
+handle_call({merge, OtherGCounter}, _From, GCounter) ->
+    MergedGCounter = riak_dt_gcounter:merge(OtherGCounter, GCounter),
+    {reply, MergedGCounter, MergedGCounter};
 handle_call(_Request, _From, State) ->
     {reply, ok, State}.
 
+handle_cast({increment, Amount}, GCounter) ->
+    {ok, NewGCounter} = riak_dt_gcounter:update({increment, Amount},
+                                                {p, node()},
+                                                GCounter),
+    {noreply, NewGCounter};
+handle_cast({decrement, Amount}, GCounter) ->
+    {ok, NewGCounter} = riak_dt_gcounter:update({increment, Amount},
+                                      {n, node()},
+                                      GCounter),
+    {noreply, NewGCounter};
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
@@ -39,6 +76,7 @@ handle_info(_Info, State) ->
     {noreply, State}.
 
 terminate(_Reason, _State) ->
+    %TODO save counter to local db
     ok.
 
 code_change(_OldVsn, State, _Extra) ->
